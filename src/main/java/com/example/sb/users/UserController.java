839a3590fb9c9c733802be6c50caaa8ec0eb8836
KangSungLee <rkdtjd1020@naver.com>
2024-03-07 09:42:10 +0900

    Day 3

--- src/main/java/com/example/sb/users/UserController.java
+++ src/main/java/com/example/sb/users/UserController.java
@@ -18,8 +18,9 @@ import jakarta.servlet.http.HttpSession;
 public class UserController {
 	@Autowired private UserService userSvc;
 	
-	@GetMapping("/list/{page}")
-	public String list(@PathVariable int page, Model model) {	//list를 던져주기위해 model사용
+	@GetMapping(value={"/list/{page}", "/list"})
+	public String list(@PathVariable(required=false) Integer page, Model model) {	//list를 던져주기위해 model사용
+		page = (page == null) ? 1 : page;
 		List<User> list = userSvc.getUserList(page);
 		model.addAttribute("userList", list);
 		return "user/list";
@@ -40,12 +41,12 @@ public class UserController {
 		return "redirect:/user/list/1";
 	}
 	
-	@GetMapping("login")
+	@GetMapping("/login")
 	public String login() {
 		return "user/login";
 	}
 	
-	@PostMapping("login")
+	@PostMapping("/login")
 	public String loginProc(String uid, String pwd, Model model, HttpSession session) {
 		String msg, url;
 		int result = userSvc.login(uid, pwd);
@@ -54,42 +55,46 @@ public class UserController {
 			session.setAttribute("sessUid", uid);
 			session.setAttribute("sessUname", user.getUname());
 			msg = user.getUname() + "님 환영합니다.";
-			url = "list/1";
+			url = "/sb/user/list/1";
 		} else if (result == userSvc.WRONG_PASSWORD) {
 			msg = "패스워드가 틀립니다.";
-			url = "login";
+			url = "/sb/user/login";
 		} else {
 			msg = "아이디 입력이 잘못되었습니다.";
-			url = "login";
+			url = "/sb/user/login";
 		}
 		model.addAttribute("msg", msg);
 		model.addAttribute("url", url);
 		return "user/alertMsg";
 	}
 	
-	@GetMapping("logout")
+	@GetMapping("/logout")
 	public String logout(HttpSession session) {
 		session.invalidate();
-		return "redirect:/user/list/1";
+		return "redirect:/user/login";
 	}
 	
-	@GetMapping("update/{uid}")
+	@GetMapping("/update/{uid}")
 	public String update(@PathVariable String uid, Model model) {
 		User user = userSvc.getUserByUid(uid);
 		model.addAttribute("user", user);
 		return "user/update";
 	}
 	
-	@PostMapping("update")
-	public String updateProc(String uid, String pwd, String pwd2, String uname, String email, String hashedPwd, Model model) {
-		if (pwd != null && pwd.equals(pwd2))
-			hashedPwd = BCrypt.hashpw(pwd, BCrypt.gensalt());
-		User user = new User(uid, hashedPwd, uname, email);
+	@PostMapping("/update")
+	public String updateProc(String uid, String pwd, String pwd2, String uname, String email) {
+		User user = userSvc.getUserByUid(uid);
+		if (pwd != null && pwd.equals(pwd2)) {
+			String hashedPwd = BCrypt.hashpw(pwd, BCrypt.gensalt());
+			user.setPwd(hashedPwd);
+		}
+		user.setUname(uname);
+		user.setEmail(email);
 		userSvc.updateUser(user);
-		return "redirect:/user/list/1";
+		return "redirect:/user/list";
 	}
 	
-	@GetMapping("delete/{uid}")
+	@GetMapping("/delete/{uid}")
 	public String delete(@PathVariable String uid, HttpSession session) {
 		userSvc.deleteUser(uid);
 //		String sessUid = (String) session.getAttribute("sessUid");
